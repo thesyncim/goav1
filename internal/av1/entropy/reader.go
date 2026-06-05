0dfb6675775986c14ec27680a9fd9f25f13016fd
thesyncim <thesyncim@gmail.com>
2026-06-05 02:44:21 +0100

    entropy: streamline cursor cdf4 update

--- internal/av1/entropy/reader.go
+++ internal/av1/entropy/reader.go
@@ -1332,23 +1332,20 @@ func (c *Cursor) readCDF4UpdateKnown(values *[MaxSymbols + 1]uint16) int {
 	}
 	count := values[4]
 	rate := uint(5 + (count >> 4))
-	switch symbol {
-	case 0:
+	if symbol > 0 {
+		values[0] = uint16(c0 + ((CDFProbTop - c0) >> rate))
+	} else {
 		values[0] = uint16(c0 - (c0 >> rate))
-		values[1] = uint16(c1 - (c1 >> rate))
-		values[2] = uint16(c2 - (c2 >> rate))
-	case 1:
-		values[0] = uint16(c0 + ((CDFProbTop - c0) >> rate))
-		values[1] = uint16(c1 - (c1 >> rate))
-		values[2] = uint16(c2 - (c2 >> rate))
-	case 2:
-		values[0] = uint16(c0 + ((CDFProbTop - c0) >> rate))
+	}
+	if symbol > 1 {
 		values[1] = uint16(c1 + ((CDFProbTop - c1) >> rate))
-		values[2] = uint16(c2 - (c2 >> rate))
-	default:
-		values[0] = uint16(c0 + ((CDFProbTop - c0) >> rate))
-		values[1] = uint16(c1 + ((CDFProbTop - c1) >> rate))
+	} else {
+		values[1] = uint16(c1 - (c1 >> rate))
+	}
+	if symbol > 2 {
 		values[2] = uint16(c2 + ((CDFProbTop - c2) >> rate))
+	} else {
+		values[2] = uint16(c2 - (c2 >> rate))
 	}
 	if count < MaxCDFCount {
 		values[4] = count + 1
