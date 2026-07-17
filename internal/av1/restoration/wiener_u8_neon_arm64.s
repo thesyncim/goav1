7f06c0b4a4b1cd1dc6e97ad922470950b441384e
thesyncim <thesyncim@gmail.com>
2026-07-17 11:37:16 +0100

    restoration: fuse Wiener byte sums with widening

--- internal/av1/restoration/wiener_u8_neon_arm64.s
+++ internal/av1/restoration/wiener_u8_neon_arm64.s
@@ -64,24 +64,23 @@ hColLoop:
 	WORD $0x4eb21e50       // mov v16.16b, v18.16b  seed lanes 0..3
 	WORD $0x4eb21e51       // mov v17.16b, v18.16b  seed lanes 4..7
 	WORD $0x4c407121       // ld1 {v1.16b}, [x9]    16 source u8
-	WORD $0x2f08a422       // uxtl  v2.8h, v1.8b    lanes 0..7  -> u16
-	WORD $0x6f08a423       // uxtl2 v3.8h, v1.16b   lanes 8..15 -> u16
 
-	WORD $0x6e036044       // ext v4.16b, v2.16b, v3.16b, #12  s6..s13
-	WORD $0x4e628484       // add v4.8h, v4.8h, v2.8h            s0+s6
+	WORD $0x6e013022       // ext v2.16b, v1.16b, v1.16b, #6
+	WORD $0x2e220024       // uaddl v4.8h, v1.8b, v2.8b          s0+s6
 	WORD $0x0f402090       // smlal  v16.4s, v4.4h, v0.h[0]
 	WORD $0x4f402091       // smlal2 v17.4s, v4.8h, v0.h[0]
-	WORD $0x6e031044       // ext v4.16b, v2.16b, v3.16b, #2   s1..s8
-	WORD $0x6e035045       // ext v5.16b, v2.16b, v3.16b, #10  s5..s12
-	WORD $0x4e658484       // add v4.8h, v4.8h, v5.8h            s1+s5
+	WORD $0x6e010822       // ext v2.16b, v1.16b, v1.16b, #1
+	WORD $0x6e012823       // ext v3.16b, v1.16b, v1.16b, #5
+	WORD $0x2e230044       // uaddl v4.8h, v2.8b, v3.8b          s1+s5
 	WORD $0x0f502090       // smlal  v16.4s, v4.4h, v0.h[1]
 	WORD $0x4f502091       // smlal2 v17.4s, v4.8h, v0.h[1]
-	WORD $0x6e032044       // ext v4.16b, v2.16b, v3.16b, #4   s2..s9
-	WORD $0x6e034045       // ext v5.16b, v2.16b, v3.16b, #8   s4..s11
-	WORD $0x4e658484       // add v4.8h, v4.8h, v5.8h            s2+s4
+	WORD $0x6e011022       // ext v2.16b, v1.16b, v1.16b, #2
+	WORD $0x6e012023       // ext v3.16b, v1.16b, v1.16b, #4
+	WORD $0x2e230044       // uaddl v4.8h, v2.8b, v3.8b          s2+s4
 	WORD $0x0f602090       // smlal  v16.4s, v4.4h, v0.h[2]
 	WORD $0x4f602091       // smlal2 v17.4s, v4.8h, v0.h[2]
-	WORD $0x6e033044       // ext v4.16b, v2.16b, v3.16b, #6
+	WORD $0x6e011822       // ext v2.16b, v1.16b, v1.16b, #3
+	WORD $0x2f08a444       // uxtl v4.8h, v2.8b                  s3
 	WORD $0x0f702090       // smlal  v16.4s, v4.4h, v0.h[3]
 	WORD $0x4f702091       // smlal2 v17.4s, v4.8h, v0.h[3]
 
